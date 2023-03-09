d0f5688d0311ff22a02931288d57301022a6ca91
BetterCallAle <dap22091998@gmail.com>
2023-03-09 14:07:34 +0100

    Snack 1 completed

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -1,7 +1,22 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack1 {
     public static void main(String[] args) {
+        /*
+            Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+        */
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inserisci un numero");
+        int userNumber = Integer.parseInt(scan.nextLine());
+        scan.close();
+
+        if (userNumber % 2 == 0){
+            System.out.println("Il numero è pari");
+        } else {
+            System.out.println("Il numero è dispari, il suo numero successivo è " + ++userNumber);
+        }
     }
 }
